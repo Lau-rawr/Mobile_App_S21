bc94a005b740c47dc1d723b9abb9535ad5f2934e
pacificPlumeria <laura.worboys22@gmail.com>
2021-02-05 22:28:57 -0800

    Clicky Clicky Assignment, also removed fragments and converted first assignment to activities instead

---
+++ app/src/main/java/edu/neu/madcourse/numsd21s_lauraworboys/AboutMe.java
@@ -0,0 +1,26 @@
+package edu.neu.madcourse.numsd21s_lauraworboys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class AboutMe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_me);
+    }
+    public void onClick(View view) {
+
+
+        switch (view.getId()) {
+            //code for each button
+            case R.id.aboutPrevious:
+                startActivity(new Intent(AboutMe.this, MainActivity.class));
+                break;
+        }
+    }
+}
